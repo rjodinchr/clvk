46dee5b1d378f275982dc57a5d84de7857b1155a
Romaric Jodin <rjodin@google.com>
2025-09-09 07:03:03 +0200

    clvk-03-GITHUB-PR-807-cl_khr_command_buffer.patch

--- src/memory.hpp
+++ src/memory.hpp
@@ -640,6 +640,8 @@ struct cvk_image : public cvk_mem {
                              std::vector<cl_mem_properties>&& properties,
                              cl_int* errcode_ret);
 
+    static cvk_image* create_write_enable_image_from(cvk_image* image);
+
     bool is_backed_by_buffer_view() const {
         return type() == CL_MEM_OBJECT_IMAGE1D_BUFFER;
     }
@@ -703,6 +705,7 @@ struct cvk_image : public cvk_mem {
     cvk_mem* buffer() const { return icd_downcast(m_desc.buffer); }
     cl_uint num_mip_levels() const { return m_desc.num_mip_levels; }
     cl_uint num_samples() const { return m_desc.num_samples; }
+    cl_image_info image_type() const { return m_desc.image_type; }
 
     bool has_same_format(const cvk_image* other) const {
         auto fmt = format();
@@ -831,9 +834,9 @@ struct cvk_image : public cvk_mem {
                               size_t* size_ret) const;
 
 private:
-    cl_int init_vulkan_image();
+    cl_int init_vulkan_image(std::shared_ptr<cvk_memory_allocation> memory);
     cl_int init_vulkan_texel_buffer();
-    cl_int init();
+    cl_int init(std::shared_ptr<cvk_memory_allocation> memory = nullptr);
 
     size_t num_channels() const {
         switch (m_format.image_channel_order) {
